4becbe43af86f8897c65f265f7ae35db8c0e817e
David Flores <earlyepitaph@gmail.com>
2018-05-28 12:29:39 -0500

    barebones file struction and mysql

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+-- Creating seed data for the burgers_db database --
+insert into burgers (burger_name, devoured) values ("Tunami Burger", false);
+insert into burgers (burger_name, devoured) values ("Olive Free or Die Hard Burger", false);
+insert into burgers (burger_name, devoured) values ("Thank Goodness It's Fried Egg Burger", false);
+insert into burgers (burger_name, devoured) values ("Poblano Picasso Burger", true);
+insert into burgers (burger_name, devoured) values ("New Bacon-ings Burger", true);
+insert into burgers (burger_name, devoured) values ("Mission A-corn plished Burger", true);
