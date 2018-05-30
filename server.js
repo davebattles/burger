2e4e08fcc52c90f1eff5412d642c39cd30ce08db
David Flores <earlyepitaph@gmail.com>
2018-05-29 22:00:49 -0500

    5th time trying to fix favicon

--- server.js
+++ server.js
@@ -2,10 +2,8 @@ var express= require('express');
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
 var exphbs = require('express-handlebars');
-var favicon = require('serve-favicon');
 var app = express();
 app.use(express.static(__dirname + '/public'));
-app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(bodyParser.urlencoded({
   extended: false
 }))
@@ -17,9 +15,7 @@ app.set('view engine','handlebars');
 var routes = require('./controllers/burgers_controller.js');
 app.use('/',routes);
 var port = 3000;
-app.get("/favicon.ico", function (req, res) {
-  res.sendFile(path.join(__dirname, "../public/favicon.ico"));
-});
+
 app.listen(port, function() {
   // Log (server-side) when our server has started
   console.log("Server listening on: http://localhost:" + port);
