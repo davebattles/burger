bd742bb2691d32b5937de7dc6e87ace229c84e7f
David Flores <earlyepitaph@gmail.com>
2018-06-04 09:56:42 -0500

    app listen 5000

--- server.js
+++ server.js
@@ -16,5 +16,5 @@ var routes = require('./controllers/burgers_controller.js');
 app.use('/',routes);
 var port = 3000;
 
-// app.listen(process.env.PORT || 5000);
-app.listen(port);
+ app.listen(process.env.PORT || 5000);
+//app.listen(port);
