3d74d5357c5541a55a6111c8fe62998a3af9240a
David Flores <earlyepitaph@gmail.com>
2018-06-04 10:02:45 -0500

    added checks

--- config/orm.js
+++ config/orm.js
@@ -15,7 +15,7 @@ var orm = {
     })
   },
   create: function(tableInput,val,cb){
-    connection.query('insert into '+tableInput+" (burger_name) values ("+val+");", function(err,result){
+    connection.query('insert into '+tableInput+" (burger_name) values ('"+val+"');", function(err,result){
       if(err)throw err;
       cb(result);
     })
